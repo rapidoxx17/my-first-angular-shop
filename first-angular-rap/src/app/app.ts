d332cef414c670fffe7d9d6294c8d292db55559d
Raphael B <brendez.rafaelkeith@gmail.com>
2026-02-21 06:52:50 +0800

    first-angular

---
+++ first-angular-rap/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class AppComponent {
+  title = 'SoleShop';
+}
+
+export { AppComponent as App };
